763e0106824ed9f9c362735651bfbb1ea1e35fcd
Alex.Net <ritm.homa@gmail.com>
2024-03-02 13:20:08 +0400

    init

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Collections;
+
+Console.WriteLine("Hello, World!");
+
+// Создание двумерного массива
+People[,] peoplesArray = new People[3, 3];
+
+// Создание словаря
+Dictionary<string, int> addresses = new Dictionary<string, int>();
+
+// Добавление элементов в массив и соответствующие ключи в словарь
+addresses.Add("address1", 2);
+peoplesArray[0, 0] = new People("s",0);
+
+addresses.Add("address2", 1);
+peoplesArray[1, 2] = new People("d",1);
+
+// Получение элемента массива по ключу из словаря
+int index;
+if (addresses.TryGetValue("address1", out index))
+{
+    People person = peoplesArray[index, 0];
+    
+}
+
+foreach (var VARIABLE in peoplesArray)
+{
+    Console.WriteLine(VARIABLE);
+}
+
--- Vendomat/Program.cs
+++ Vendomat/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Vendomat.Model;
 
-Console.WriteLine("Hello, World!");
+var currecy = new List<Currency>
+{
+    new Banknote(3),
+    new Banknote(10),
+    new Coin(10),
+};
+
+var products = new List<Product>
+{
+    new(Guid.NewGuid(), "Bread", 10),
+    new(Guid.NewGuid(), "Milk", 1),
+};
+var vendomat = new Vendomat.Model.Vendomat(products, currecy);
+
+Console.WriteLine(vendomat._bank.GetTotalBalance());
+Console.WriteLine(vendomat._bank.GetBanknoteBalance());
+Console.WriteLine(vendomat._bank.GetCoinBalance());
+Console.WriteLine();
